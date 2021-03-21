08e4ded82b974d1f0fec2c31099c70fb5fb589d1
Igor-A-Penkin <penkin.igor@gmail.com>
2021-03-21 18:18:25 +0300

    task2
    Remade with inline output

--- ExamplesObjC/ExamplesObjC/main.m
+++ ExamplesObjC/ExamplesObjC/main.m
@@ -21,14 +21,14 @@ int main(int argc, const char * argv[]) {
         printf("Second number: ");
         scanf("%lf", &second);
 
-        NSLog(@"\nMax: %f", [mathClass maxOf:first and:second]);
-        NSLog(@"\nSquare first: %f", [mathClass square:first]);
-        NSLog(@"\nSquare second: %f", [mathClass square:second]);
-        NSLog(@"\nSum: %f", [mathClass sumOf:first and:second]);
-        NSLog(@"\nSubtruction: %f", [mathClass subtructFrom:first value:second]);
-        NSLog(@"\nDivision: %f", [mathClass divide:first by:second]);
-        NSLog(@"\nMultiplication: %f", [mathClass multiply:first by:second]);
-        NSLog(@"\nRemainder of division: %ld",
+        NSLog(@"\nMax: %f; Square first: %f; Square second: %f; Sum: %f; Subtruction: %f; Division: %f; Multiplication: %f; Remainder of division: %ld;",
+              [mathClass maxOf:first and:second],
+              [mathClass square:first],
+              [mathClass square:second],
+              [mathClass sumOf:first and:second],
+              [mathClass subtructFrom:first value:second],
+              [mathClass divide:first by:second],
+              [mathClass multiply:first by:second],
               (long)[mathClass remainderOf:(NSInteger)first
                                  dividedBy:(NSInteger)second]);
     }
