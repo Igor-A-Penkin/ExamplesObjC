02afb14d65838466850309fe2397ff438d369ddb
Igor Penkin <69014236+Igor-A-Penkin@users.noreply.github.com>
2021-04-13 13:27:27 +0300

    Task1: printing strings with cycles

--- ExampleObjC/ExampleObjC/ViewController.m
+++ ExampleObjC/ExampleObjC/ViewController.m
@@ -9,13 +9,59 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textInput;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+@property (weak, nonatomic) IBOutlet UIButton *printButton;
+
+@property (nonatomic) NSMutableArray<NSString *> *strings;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _strings = [NSMutableArray new];
     
+}
+
+- (IBAction)saveButtonPressed:(id)sender {
+    [_strings addObject:_textInput.text];
+    _textInput.text = @"";
+}
+
+- (IBAction)printButtonPressed:(id)sender {
+    [self printWithForIn];
+    [self printWithWhile];
+    [self printWithDoWhile];
+}
+
+- (void)printWithForIn {
+    NSLog(@"Printed with FOR-IN");
+    for (NSString *string in _strings) {
+        NSLog(@"- %@", string);
+    }
+}
+
+- (void)printWithWhile {
+    NSLog(@"Printed with WHILE");
+    NSInteger count = 0;
+    while (count < _strings.count) {
+        NSLog(@"- %@", _strings[count]);
+        count+=1;
+    }
+}
+
+- (void)printWithDoWhile {
+    if (_strings.count == 0) {
+        return;
+    }
+    NSLog(@"Printed with DO-WHILE");
+    NSInteger count = 0;
+    do {
+        NSLog(@"- %@", _strings[count]);
+        count+=1;
+    } while (count < _strings.count);
 }
 
 @end
