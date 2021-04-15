4877f0ed74f1e921c5e68f1787ebd086e447ed49
Igor Penkin <69014236+Igor-A-Penkin@users.noreply.github.com>
2021-04-15 12:09:23 +0300

    Empty project

---
+++ ExampleObjC/ExampleObjC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ExampleObjC
+//
+//  Created by Игорь Пенкин on 25.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
