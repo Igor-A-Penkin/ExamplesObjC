0a3046efbf491b06914390bece83ddf2bd9b5875
Igor Penkin <69014236+Igor-A-Penkin@users.noreply.github.com>
2021-04-23 14:35:04 +0300

    Empty project

---
+++ ExampleObjC/ExampleObjC/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ExampleObjC
+//
+//  Created by Игорь Пенкин on 25.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ExampleObjC/ExampleObjC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ExampleObjC
+//
+//  Created by Игорь Пенкин on 25.03.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
